8ad9dd4cb2650d3d0173665ce909f21a457d9ce8
ElenaS35 <helena0518savitskaya@gmail.com>
2024-02-26 10:54:50 +0200

    change comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const cols = document.querySelectorAll('.col')
 
-// переключение цвета путем нажатия пробела
+// switch color by pressing spacebar
 document.addEventListener('keydown', (event) => {
   event.preventDefault()
   if (event.code.toLowerCase() === 'space') {
@@ -9,30 +9,30 @@ document.addEventListener('keydown', (event) => {
 })
 
 document.addEventListener('click', (event) => {
-  // слушаю событие клик и ищу именно те элементы на странице, у которых есть data-type 'lock'
+  // listen to the click event and look for exactly those elements on the page that have data-type 'lock'
   const type = event.target.dataset.type
 
   if (type === 'lock') {
 
-    // теперь нужно менять класс именно у тега i
+    // now it is necessary to change the class specifically for the 'i' tag
     const node =
-      // сразу перевожу в нижний регистр
-      // а если это была кнопка, то нужно получить первого ребенка
-      // теперь если посмотреть на node - это всегда иконка
+      // immediately convert to lowercase
+      // and if it was a button, then you need to get the first child
+      // now if you look at the node - it's always an icon
       event.target.tagName.toLowerCase() === 'i'
         ? event.target
         : event.target.children[0]
 
-    // при помощи метода toggle меняю класс
+    // using the toggle method to change the class
     node.classList.toggle('fa-lock-open')
     node.classList.toggle('fa-lock')
   } else if (type === 'copy') {
-    copyToClickboard(event.target.textContent)
+    copyToClipboard(event.target.textContent)
   }
 })
 
-// функция получения рандомных цветов
-function gerenerateRandomColor() {
+// function to get random colors
+function generateRandomColor() {
   // RGB
   // #FF0000
   // #00FF00
@@ -47,20 +47,20 @@ function gerenerateRandomColor() {
 }
 
 
-// функция копирования по клику
-function copyToClickboard(text) {
+// function to copy on click
+function copyToClipboard(text) {
   return navigator.clipboard.writeText(text)
 }
 
-// подключаю в проект библиотеку chroma
+// connecting chroma library to the project
 
 
-// функция выведения рандомных цветов
+// function to output random colors
 function setRandomColors(isInitial) {
   const colors = isInitial ? getColorsFromHash() : []
 
   cols.forEach((col, index) => {
-    // если иконка заблокирована - не менять цвет
+    // if the icon is locked - do not change the color
     const isLocked = col.querySelector('i').classList.contains('fa-lock')
 
     const text = col.querySelector('h2')
@@ -96,8 +96,8 @@ function setTextColor(text, color) {
   text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
-// создую массив цветов для того, чтобы можно было передать дизайнеру
-// создав переменную colors в нее записываю цвета в функции setRandomColors
+// creating an array of colors so that it can be passed to the designer
+// by creating a colors variable, I write colors in the setRandomColors function
 function updateColorsHash(colors = []) {
   document.location.hash = colors
     .map((col) => {
@@ -106,7 +106,7 @@ function updateColorsHash(colors = []) {
     .join('-')
 }
 
-// получение цветов из хеша
+// getting colors from the hash
 function getColorsFromHash() {
   if (document.location.hash.length > 1) {
     return document.location.hash
